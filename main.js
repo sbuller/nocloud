425a664de17dbe766fdb987a2acab8105dfec6c4
Stefan Buller <stefan_buller@nova3.ca>
2017-03-15 14:37:56 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const FAT = require('nos-mkfat')
+const fs = require('fs')
+const tmp = require('tmp')
+const concat = require('concat-stream')
+const yaml = require('js-yaml')
+
+let fat = new FAT({name:'cidata'})
+let out = tmp.fileSync()
+
+process.stdin.pipe(concat(config=>{
+	let configObj = JSON.parse(config)
+
+	let user = Buffer.from(yaml.safeDump(configObj.user || {}))
+	let meta = Buffer.from(yaml.safeDump(configObj.meta || {}))
+
+	fat.entry({name:'user-data', size:user.length}, user)
+	fat.entry({name:'meta-data', size:meta.length}, meta)
+
+	function pipeOut(fd) {
+		let bar = fs.createReadStream(null, {fd})
+		bar.pipe(process.stdout)
+	}
+
+	fat.makeDisk(out.fd).then(()=>pipeOut(out.fd)).catch(console.err)
+}))
